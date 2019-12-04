89796388db2900025d08d3efd65632f460190120
49497974m <49497974tkhsm>
2019-12-04 12:46:19 +0900

    Defining test data

--- RSpec_project/spec/factories/users.rb
+++ RSpec_project/spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    email { "MyString" }
+    sequence(:name) { |n| "TEST_NAME#{n}" }
+    sequence(:email) { |n| "TEST_EMAIL#{n}@example.com" }
   end
 end
